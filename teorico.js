3596beba6455ebd3ddb74f3c3a83946f457e09cd
Thay7 <thay3x@gmail.com>
2022-11-06 18:17:38 -0300

    add teorico

--- teorico.js
+++ teorico.js
@@ -15,3 +15,32 @@ if (verificacao) {
 } else {
     console.log('É menor de idade')
 }
+
+
+
+
+//arrow function
+const nomeDaFuncao = (parametro1, parametro2) => {
+    return algo
+}
+//mais resumida (para quando retorna só uma linha)
+const somar = (x, y) => x + y
+
+//quando só tem um parametro não precisa dos parenteses
+const sobrenome = sob => 'Thay ' + sob
+
+
+
+
+//variaveis dentro de funções
+let count = 0
+
+function add() {
+    let count = 0
+    count++
+}
+
+add()
+add()
+
+console.log(count) //dentro das funções, variaveis com mesmo nome, as locais sobrepõem variaveis as globais, dessa forma aqui retorna 0, se comentamos a linha 39, aqui retorna 2
