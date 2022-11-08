257c9069f263cb3d8dd4c2195fbe4a8ea88d9a5a
Thay7 <thay3x@gmail.com>
2022-11-07 22:06:27 -0300

    add files

---
+++ Loops/Exercicies/execiciesLoops.js
@@ -0,0 +1,13 @@
+//1. Faça um loop que mostre todas as frutas
+let frutas = ['Maça', 'Uva', 'Banana']
+
+for(let fruta of frutas) {
+    console.log(fruta)
+}
+
+//2. Conte de 1 até 100 através de um WHILE
+let i = 1;
+while (i <= 100) {
+    console.log(i)
+    i++
+}
